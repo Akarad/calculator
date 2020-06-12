c1890d7ab8bfa4325f8c357b482b8b893a91e840
Diana <dianaakara@gmail.com>
2020-06-12 15:55:43 +0300

    add div to disply results on html file and front-end logic code to gather the inputed data

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,11 @@ var divide = function(number1, number2){
 
 //Everything below this line is user interface (or front end) logic:
 $(document).ready(function(){
+  $("form#add").submit(function(event){
+    event.preventDefault();
   var number1 = parseInt($("#add1").val());
   var number2 = parseInt($("#add2").val());
-  alert(add(number1,number2));
+  var result = add(number1,number2);
+  $("#output").text(result);
+  });
 });
