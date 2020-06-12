efc5d088464ce83720b7ce8122b9aa240f3da85d
Diana <dianaakara@gmail.com>
2020-06-12 15:03:04 +0300

    add methods to jQuery file that collects the useres inputed data

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// Business (or back-end) logic:
 var add = function(number1,number2){
 return(number1 + number2)
 }
@@ -10,6 +11,10 @@ var multiply = function(number1,number2){
 var divide = function(number1, number2){
   return(number1 / number2)
 }
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-alert(multiply(number1,number2));
+
+//Everything below this line is user interface (or front end) logic:
+$(document).ready(function(){
+  var number1 = parseInt($("#add1").val());
+  var number2 = parseInt($("#add2").val());
+  alert(add(number1,number2));
+});
